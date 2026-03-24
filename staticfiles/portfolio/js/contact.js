a7c3247abb466e2f57517725094312e5d61cb2be
david-kajinake <davidkajinakeschenezare@gmail.com>
2026-03-25 01:52:10 +0300

    Added staticfiles

---
+++ staticfiles/portfolio/js/contact.js
@@ -0,0 +1,5 @@
+const nameField = document.getElementById("name");
+const emailField = document.getElementById("email");
+const messageField = document.getElementById("message");
+
+const contactForm = document.getElementById("contact-form");
